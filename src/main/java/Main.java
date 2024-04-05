38dac53e6e36d3cbc5556725ea422e0f58112f88
aiswlz <230787@astanait.edu.kz>
2024-04-05 11:42:44 +0600

    Task1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int min = arr[0];
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] < min) {
-                min = arr[i];
+                min =arr[i];
             }
         }
         return min;
